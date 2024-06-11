0a61fd2882912c152db4d663d91242e93434b7d0
Asad Rao <asad.mukeem@ssssco.com>
2024-06-11 14:24:19 +0400

    updating config name.

--- app/src/main/java/com/android/app/fakechatapp/activities/chat_screen/ChatActivity.kt
+++ app/src/main/java/com/android/app/fakechatapp/activities/chat_screen/ChatActivity.kt
@@ -265,6 +265,10 @@ class ChatActivity : AppCompatActivity() {
         }
     }
 
+    private fun method() {
+
+    }
+
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
